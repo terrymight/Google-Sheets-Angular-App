62a4f9742b1f251f5a4fe87b711fd0b3ca43dc9b
Tejiri Mayone <terrymayone@gmail.com>
2025-06-17 23:20:36 +0100

    Ignore environment config files

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  googleClientId: '314245803123-3dbdcrcggfu7mlvr3cbo8j6ker6r1ubj.apps.googleusercontent.com',
-  spreadsheetId: '199HGlHYXHx8tNl00YWrknxfNmmndXLYnPDlYD8d-GdU',
-  // apiKey: 'AIzaSyCpiTKeMSY6L6aRC_DZeUJjotrUmHP3614'
-};
