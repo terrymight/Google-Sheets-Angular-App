a6b52fd841f91efadfb1abca2424f53a101279f4
Tejiri Mayone <terrymayone@gmail.com>
2025-06-18 23:00:53 +0100

    publish app for the first time on firsbase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: ()=> import('./components/sigin/sigin').then( m => m.Sigin) },
-    { path: 'staff-form', loadComponent: ()=> import('./components/staff_form/staff-performance-form').then(m=>m.StaffPerformanceForm) },
+    // { path: '', loadComponent: ()=> import('./components/sigin/sigin').then( m => m.Sigin) },
+    { path: '', loadComponent: ()=> import('./components/staff_form/staff-performance-form').then(m=>m.StaffPerformanceForm) },
     { path: '**', redirectTo: '' }
 ];
